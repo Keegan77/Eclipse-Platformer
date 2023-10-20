18edd52cacf6740ebc45d2cdc5f9b6da266249e1
D-E-I-G <92387839+D-E-I-G@users.noreply.github.com>
2023-10-19 21:59:46 -0400

    Added menu for start
    Looooove you Keegan

---
+++ Assets/Menu and Buttons/Change_Image.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Change_Image : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Sprite newButtonImage;
+    public Button button;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeButtonImage()
+    {
+        button.image.sprite = newButtonImage;
+    }
+
+}
